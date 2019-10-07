45e32b4cb2143e12d20eda77649d9dd14464a217
Griffo, G <g.griffo@accenture.com>
2019-10-07 13:40:46 -0500

    first two projects

---
+++ Birthday-Chocolate-Problem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday_Chocolate_Problem
+{
+    class Program
+    {
+        static int Birthday(List<int> s, int d, int m)
+        {
+            int result = 0;
+            for (int i = 0; i < s.Count; i++)
+            {
+                if (i <= s.Count - m)
+                {
+                    if (d == s.Skip(i).Take(m).Sum())
+                        result++;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            //hackerrank link: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+            //TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+            string[] dm = Console.ReadLine().TrimEnd().Split(' ');
+
+            int d = Convert.ToInt32(dm[0]);
+
+            int m = Convert.ToInt32(dm[1]);
+
+            int result = Birthday(s, d, m);
+
+            Console.WriteLine(result);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
---
+++ Divisible-Sum-Pairs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisible_Sum_Pairs
+{
+    class Program
+    {
+        // Complete the divisibleSumPairs function below.
+        static int divisibleSumPairs(int n, int k, int[] ar)
+        {
+            int result = 0;
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = i+1; j < ar.Length; j++)
+                {
+                    if (ar[i] + ar[j] == k || ((ar[i] + ar[j]) % k) == 0)
+                        result++;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = divisibleSumPairs(n, k, ar);
+
+            Console.WriteLine(result);
+        }
+    }
+}
